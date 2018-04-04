a38bda4139c6e538416b52afeb6632faf75982d4
Shawn Anderson <shawn_anderson@sfu.ca>
2018-04-04 14:33:09 -0700

    Evaluator module.

--- CryptViz/DeepLearning/Evaluator.py
+++ CryptViz/DeepLearning/Evaluator.py
@@ -12,12 +12,20 @@ import pandas as pd
 class Evaluator:
     """I am a coin evaluator"""
 
+    def __init__(self, pred_type='binary'):
+        self.pred_type = pred_type
+
+    def softmax(self, x):
+        """Compute softmax values for each sets of scores in x."""
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0) 
+
     def daterange(self, start_date, end_date, first_date):
         start = (start_date-first_date).days
         for i, n in enumerate(range(start, (end_date - start_date).days+start)):
             yield (n, start_date + timedelta(i))
 
-    def split_samples(self, pt, daterange, day_block=60, days_future=1, pred_type='binary'):
+    def split_samples(self, pt, daterange, day_block=60, days_future=1):
         for t, _ in daterange:
             X = pt[:,t:t+day_block,:].copy()
             y = pt[:,t+day_block+days_future,3].copy() # 3 is the index of closing price in features
@@ -33,12 +41,12 @@ class Evaluator:
             
             y = y / current_prices
 
-            if pred_type == 'binary':
+            if self.pred_type == 'binary':
                 # Convert to binary up or down movement
                 y = (y > 1).astype(int)
-            if pred_type == 'stochastic':
+            if self.pred_type == 'stochastic':
                 # Convert to stochastic vector
-                y = (y > 1).astype(int)
+                y = self.softmax(y.values.astype(float))
             
             yield (X, y)
 
@@ -50,22 +58,26 @@ class Evaluator:
 
 class CNN(Evaluator):
     """I am a CNN coin evaluator"""
-    def __init__(self, activation='Sigmoid'):
-        self.model = self.init_model()
-        self.activation = activation
-        if activation == 'Sigmoid':
-            self.loss = 'binary_crossentroyp'
-        else:
+    def __init__(self, pred_type='binary'):
+        if pred_type == 'binary':
+            self.pred_type = 'binary'
+            self.activation = 'sigmoid'
+            self.loss = 'binary_crossentropy'
+        elif pred_type == 'stochastic':
+            self.pred_type = 'stochastic'
+            self.activation = 'softmax'
             self.loss = 'mean_squared_error'
+        self.model = self.init_model()
         return
 
     def init_model(self):
-        sgd = SGD(lr=0.1, decay=1e-6, momentum=1.9)
+        sgd = SGD(lr=0.1, decay=1e-6, momentum=1.9, nesterov=True)
 
         model = Sequential()
         model.add(Conv2D(filters=4, kernel_size=(1, 3), input_shape=[100,60,8]))
         model.add(Activation('relu'))
-        model.add(Conv2D(20, (1,58)))
+        model.add(MaxPooling2D(pool_size=(1,2)))
+        model.add(Conv2D(20, (1,29)))
         model.add(Activation('selu'))
         model.add(Conv2D(1, (1,1)))
         model.add(Flatten())
